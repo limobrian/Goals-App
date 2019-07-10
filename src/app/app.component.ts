1a649409a88ee4dd91a8dcef6a4a91d5fa9be1de
briankiplimo <limobrian290@gmail.com>
2019-07-10 13:00:33 +0300

    goal app

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import {Goal} from './goal';
+// import {Goals} from './goal'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  // constructor(){
+  //   this.goals =[
+  //       new Goal(1,'Watch Finding Nemo'),
+  //       new Goal(2,'Buy Cookies'),
+  //       new Goal(3, 'Get new Phone Case'),
+  //       new Goal(4, 'Get Dog Food'),
+  //       new Goal(5, 'Solve math homework'),
+  //       new Goal(6, 'Plot my world domination plan'),
+  //
+  //   ];
+  //
+  //
+  // }
+  // goals = [
+  //     new Goals(1,'Watch Finding Nemo'),
+  //     new Goals(2,'Buy Cookies'),
+  //     new Goals(3, 'Get new Phone Case'),
+  //     new Goals(4, 'Get Dog Food'),
+  //     new Goals(5, 'Solve math homework'),
+  //     new Goals(6, 'Plot my world domination plan'),
+  //     new Goals(7, 'Joflix'),
+  //
+  // ]
+// }
+// import { Component } from '@angular/core';
+// import {Goals} from './goal'
+//
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html'
+// })
+// export class AppComponent {
+//     goals = [
+//         new Goals(1,'Watch Finding Nemo'),
+//         new Goals(2,'Buy Cookies'),
+//         new Goals(3, 'Get new Phone Case'),
+//         new Goals(4, 'Get Dog Food'),
+//         new Goals(5, 'Solve math homework'),
+//         new Goals(6, 'Plot my world domination plan'),
+//         new Goals(7, 'Joflix'),
+//
+//     ]
+// }
+}
